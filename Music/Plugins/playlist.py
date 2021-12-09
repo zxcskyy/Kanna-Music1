2e553b1621f8b20671935470baa5c941c6db41b0
zxcskyy <92031643+zxcskyy@users.noreply.github.com>
2021-12-10 01:54:01 +0700

    Update playlist.py

--- Music/Plugins/playlist.py
+++ Music/Plugins/playlist.py
@@ -105,7 +105,7 @@ options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", 
 
 @app.on_message(filters.command("playlist"))
 async def pause_cmd(_, message):
-    thumb ="cache/photo_2021-11-20_01-01-55.jpg"
+    thumb ="cache/IMG_20211203_150634_304.jpg"
     await message.reply_photo(
     photo=thumb, 
     caption=("**__Music's Playlist Feature__**\n\nSelect The Playlist, You want to check!"),    
