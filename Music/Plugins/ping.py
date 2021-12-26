1c40fb81c0ff6aa257e986e8e7daa9dd7fbb51ac
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-26 18:53:57 +0700

    Update ping.py

--- Music/Plugins/ping.py
+++ Music/Plugins/ping.py
@@ -25,7 +25,7 @@ Disk: {disk}%'''
     return stats
 
 
-@app.on_message(filters.command(["mping", f"ping@{BOT_USERNAME}"]))
+@app.on_message(filters.command(["ping", f"ping@{BOT_USERNAME}"]))
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
@@ -35,4 +35,4 @@ async def ping(_, message):
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_text(f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>📜 Prime Music System Stats:</u></b>{uptime}")
+    await response.edit_text(f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>📜 Music System Stats:</u></b>{uptime}")
