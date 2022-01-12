169cde9eb807b91a4a18aed23d82c7f689a04060
zxcskyy <92031643+zxcskyy@users.noreply.github.com>
2022-01-12 14:21:09 +0700

    Update db.py

--- Music/Plugins/db.py
+++ Music/Plugins/db.py
@@ -1,13 +1,13 @@
-from Music import app, SUDOERS
+import asyncio
+from Music import app, OWNER
 from pyrogram import filters, Client
 from pyrogram.types import Message
-import asyncio
 from Music.MusicUtilities.database.chats import (get_served_chats, is_served_chat, add_served_chat, get_served_chats)
-from Music.MusicUtilities.helpers.filters import command
+from ..MusicUtilities.helpers.filters import command
 
 
-@app.on_message(filters.command("broadcast") & filters.user(SUDOERS))
-async def broadcast_message(_, message):
+@app.on_message(filters.command("broadcast_pin") & filters.user(OWNER))
+async def broadcast_message_pin(_, message):
     if not message.reply_to_message:
         pass
     else :
@@ -31,10 +31,10 @@ async def broadcast_message(_, message):
                 sent += 1
             except Exception:
                 pass
-        await message.reply_text(f"**Broadcasted Message In {sent}  Chats with {pin} Pins.**")  
+        await message.reply_text(f"✅ **Pesan yang disiarkan di {sent} obrolan\n\n📌 dengan {pin} pin.**")  
         return
     if len(message.command) < 2:
-        await message.reply_text("**Usage**:\n/broadcast [MESSAGE]")
+        await message.reply_text("**Penggunaan**:\n/broadcast (message)")
         return  
     text = message.text.split(None, 1)[1]
     sent = 0
@@ -55,4 +55,46 @@ async def broadcast_message(_, message):
             sent += 1
         except Exception:
             pass
-    await message.reply_text(f"**Broadcasted Message In {sent} Chats and {pin} Pins.**")
+    await message.reply_text(f"✅ **Pesan yang disiarkan di {sent} obrolan\n📌 dengan {pin} pin.**")
+
+
+@app.on_message(filters.command("broadcast") & filters.user(OWNER))
+async def broadcast_message_nopin(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"✅ **Pesan yang disiarkan dalam {sent} obrolan")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n/broadcast (message)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"✅ **Pesan yang disiarkan dalam {sent} obrolan")
